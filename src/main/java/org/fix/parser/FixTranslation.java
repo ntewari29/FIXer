a0646f2bba1ea1b552738bbb9e05a408ffd88410
Nishant Tewari <nishant.tewari29@gmail.com>
2020-08-01 19:32:26 +0800

    Added compareFIX feature to Main class.

--- src/main/java/org/fix/parser/FixTranslation.java
+++ src/main/java/org/fix/parser/FixTranslation.java
@@ -8,9 +8,33 @@ public class FixTranslation {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter Raw Fix: ");
-        String rawMsg = br.readLine();
-        String parsedMsg = Fixer.translate(rawMsg);
-        System.out.println("\n" + "Parsed FIX Message\n" + "*******************\n" + parsedMsg);
+        System.out.println("------------------------\n" +
+                "|     FIXer Menu       |\n" +
+                "------------------------\n" +
+                "|Parse FIX Messages (1)|\n" +
+                "|Compare FIX Msgs   (2)|\n" +
+                "|                      |\n" +
+                "------------------------");
+        System.out.println("Enter your choice :: ");
+        String choice = br.readLine();
+        if (choice.equals("1")) {
+            System.out.println("Enter Raw Fix: ");
+            String rawMsg = br.readLine().trim();
+            String parsedMsg = Fixer.translate(rawMsg);
+            System.out.println("\n" + "Parsed FIX Message\n" + "*******************\n" + parsedMsg);
+        } else if (choice.equals("2")){
+            System.out.println("Enter 1st Fix: ");
+            String f1 = br.readLine().trim();
+            System.out.println("Enter 2nd Fix: ");
+            String f2 = br.readLine().trim();
+            String res = Fixer.compareFix(Fixer.convertToAMap(f1), Fixer.convertToAMap(f2));
+            if (res.equals("")){
+                System.out.println("No mismatches reported");
+            } else {
+                System.out.println(res);
+            }
+        } else {
+            System.out.println("Incorrect Option :: Exiting!");
+        }
     }
 }
